027deb496b5e7fb25ddfbcb342e1bb5eeb262ec2
Ben <bennybill16@gmail.com>
2021-05-31 14:03:06 +1000

    This is the game version of cops and robbers with ai.

---
+++ Release/assets/mapPack_tilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="mapPack_tilesheet" tilewidth="32" tileheight="32" tilecount="816" columns="34">
+ <image source="../Tilesheet/mapPack_tilesheet.png" width="1088" height="768"/>
+</tileset>
